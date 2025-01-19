c5b031f84aa6e6f92523f7ede5550e601a326c87
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-01-18 21:13:42 -0500

    Update indexAlt.js

--- indexAlt.js
+++ indexAlt.js
@@ -3,7 +3,8 @@ const { Client } = require("@notionhq/client");
 
 const notion = new Client({ auth: process.env.NEW_PERSON_KEY }); 
 
-(async () => {
+exports.getBlockData = async function (){
+  return {(async () => {
   const blockId = '17e45c5d-7d88-800f-ad35-eadd27b994f0';
     // const blockId = process.env.SAMPLE_BLOCK_ID;
   const response = await notion.blocks.children.list({
@@ -11,4 +12,7 @@ const notion = new Client({ auth: process.env.NEW_PERSON_KEY });
     page_size: 50,
   });
   console.log(response);
-})();
+})()}
+}
+  
+
